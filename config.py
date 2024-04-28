d035ea78396a77b6d085eb68aad9159f928936ab
Obed Dzikunu <okdzikunu@gmail.com>
2024-04-28 08:37:18 +0000

    Set path for dataset

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-#%%
 '''
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 '''
+#%%
 import os 
 
-PSMA_SEGMENTATION_FOLDER = '/data/blobfuse/PSMA_PCA_LESIONS_SEGMENTATION/psma_segmentation/'
+PSMA_SEGMENTATION_FOLDER = '/data/blobfuse/PSMA_PCA_LESIONS_SEGMENTATION/data_resampled_results/'
 
 DATA_FOLDER = os.path.join(PSMA_SEGMENTATION_FOLDER, 'data')
 RESULTS_FOLDER = os.path.join(PSMA_SEGMENTATION_FOLDER, 'results')
