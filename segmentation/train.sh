26c71c43abd757c7c80b755d42e90f7d256b376d
Obed Dzikunu <okdzikunu@gmail.com>
2024-04-28 08:55:17 +0000

    Change train file

--- segmentation/train.sh
+++ segmentation/train.sh
@@ -1,4 +1,2 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
 
-torchrun --standalone --nproc_per_node=1 trainddp.py --fold=0 --network-name='unet' --epochs=300 --input-patch-size=128 --train-bs=8 --num_workers=4 --lr=2e-4 --wd=1e-5 --val-interval=2 --sw-bs=8 --cache-rate=1
+/anaconda/envs/lymphoma_seg/bin/torchrun --standalone --nproc_per_node=1 trainddp_mpt.py --fold=0 --network-name='unet' --epochs=1000 --input-patch-size=128 --train-bs=8 --num_workers=4 --lr=2e-4 --wd=1e-5 --val-interval=2 --sw-bs=4 --cache-rate=1
